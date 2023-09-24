91c69e94ef75f17e6f315fc78ea420bffa94725d
harrieslloyd <92803503+harrieslloyd@users.noreply.github.com>
2023-09-24 13:27:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var i = 0
 
+function vh(percent) {
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  return (percent * h) / 100;
+}
+
+function vw(percent) {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  return (percent * w) / 100;
+}
+
 $( "#button" ).click(function() {
     gsap.to("#button", { 
         duration: 0.2,
@@ -8,7 +18,7 @@ $( "#button" ).click(function() {
       });
       gsap.to("#button", { 
         duration: 0.2,
-        width:50vh,
+        width:vh(50),
         rotation: 0,
     });
     if(Math.floor(Math.random() * 2) == 1) {
